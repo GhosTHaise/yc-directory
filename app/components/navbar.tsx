682f31be406c0a859636bd68f8d92e1e1f901c19
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-10-30 19:30:49 +0300

    feat : form action

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,17 +1,44 @@
+import { auth, signIn , signOut } from '@/auth'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-const Navbar = () => {
-  return (
-    <div className='px-5 py-3 bg-white shadow-sm font-work-sans'>
-        <nav className='flex justify-between items-center'>
-            <Link href='/'>
-                <Image loading='lazy' src='/logo.svg' width={144} height={30} alt='logo' /> 
-            </Link>
-        </nav>
-    </div>
-  )
+const Navbar = async () => {
+    const session = await auth()
+    return (
+        <div className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+            <nav className='flex justify-between items-center'>
+                <Link href='/'>
+                    <Image loading='lazy' src='/logo.svg' width={144} height={30} alt='logo' />
+                </Link>
+
+                <div className='flex items-center gap-5 text-black'>
+                    {
+                        session && session?.user ? (
+                            <>
+                                <Link href='/startup/create'>
+                                    <span>Create</span>
+                                </Link>
+                                <button onClick={signOut}>
+                                    <span>Logout</span>
+                                </button>
+                                <Link href={`/user/${session?.user?.id}`}>
+                                    <span>{session?.user?.name}</span>
+                                </Link>
+                            </>
+                        ) : (
+                            <form onClick={async () => {
+                                "use server";
+                                await signIn('github')
+                            }}> 
+                                <button type='submit'>Login</button>
+                            </form>
+                        )
+                    }
+                </div>
+            </nav>
+        </div>
+    )
 }
 
 export default Navbar
