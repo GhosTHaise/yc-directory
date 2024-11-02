3a2c89a0e608df735ea8e1d84a9df2e6273ed4e2
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-11-02 20:11:19 +0300

    feat : card grid

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,23 @@
 import SearchForm from "@/components/searchForm";
+import { title } from "process";
 
 export default async function Home({ searchParams} : {
   searchParams : Promise<{query? : string}>
 }) {
 
   const query = (await searchParams).query;
-
+  const posts = [
+    {
+      _createAt: "2023-01-01T00:00:00.000Z",
+      views : 55 ,
+      author : { _id : 1},
+      id  :1,
+      description : "desc",
+      image : "https://images.unsplash.com/photo-1646876723939-6d1f7a6a3f5d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1350&q=80",
+      category : "Category",
+      title : "robots --- 0 "
+    }
+  ]
   return (
     <>
       <section className="pink_container">
@@ -15,6 +27,19 @@ export default async function Home({ searchParams} : {
         </p>
 
         <SearchForm query={query}  />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {
+            query ? `Search results for ${query}` : "All Startups"
+          }
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {
+            !!posts.length 
+          }
+        </ul>
       </section>
     </>
   );
