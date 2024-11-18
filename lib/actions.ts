e9375ca80ed30638614b4f4996ee9c6a78c3da22
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-11-18 21:25:32 +0300

    feat : install slugify

--- lib/actions.ts
+++ lib/actions.ts
@@ -6,7 +6,9 @@ import { parseServerActionResponse } from "./utils";
 export const createPitch = async (state : any , form : FormData , pitch : string) => {
     const session = await auth()
 
-    if(!session) return parseServerActionResponse({error : 'Not signed in', status : "ERROR"});
+    if (!session) {
+      return parseServerActionResponse({error : 'Not signed in', status : "ERROR"});
+    }
 
     const {title, decription , category} = Object.fromEntries(Array.from(form).filter(([key]) => key !== 'pitch'));
 
