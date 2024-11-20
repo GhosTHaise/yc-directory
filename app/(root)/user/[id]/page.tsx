ab07b71ac991e057ef27bfa0960b7eeee094e051
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-11-20 21:05:25 +0300

    feat : fetch user posts

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,16 +1,20 @@
 import { auth } from '@/auth'
+import UserStartups, { StartupCardSkeleton } from '@/components/userStartups';
 import { client } from '@/sanity/lib/client';
 import { AUTHOR_BY_ID_QUERY } from '@/sanity/lib/query';
+import Image from 'next/image';
 import { notFound } from 'next/navigation';
-import React from 'react'
+import React, { Suspense } from 'react'
+
+export const experimental_ppr = true;
 
 const UserPage = async ({ params }: { params: Promise<{ id: string }> }) => {
     const id = (await params).id;
     const session = await auth();
 
-    const user = await client.fetch(AUTHOR_BY_ID_QUERY, {id})
+    const user = await client.fetch(AUTHOR_BY_ID_QUERY, { id })
 
-    if(!user) return notFound();
+    if (!user) return notFound();
 
     return (
         <>
@@ -21,6 +25,33 @@ const UserPage = async ({ params }: { params: Promise<{ id: string }> }) => {
                             {user.name}
                         </h3>
                     </div>
+                    <Image
+                        src={user.image ?? ""}
+                        alt={user.name ?? ""}
+                        width={220}
+                        height={220}
+                        className='profile_image' />
+
+                    <p className='text-30-extrabold mt-7 txt-center'>
+                        @{user.username}
+                    </p>
+                    <p className='mt-1 text-center text-14-normal'>
+                        {user.bio}
+                    </p>
+                </div>
+
+                <div className='flex-1 flex flex-col justify gap-5 lg:-mt-5'>
+                    <p className='text-30-bold'>
+                        {
+                            session?.id === id ? 'Your' : 'All'
+                        } Startups
+                    </p>
+
+                    <ul className='card_grid-sm'>
+                        <Suspense fallback={<StartupCardSkeleton />}>
+                            <UserStartups id={id} />
+                        </Suspense>
+                    </ul>
                 </div>
             </section>
         </>
--- sanity/types.ts
+++ sanity/types.ts
@@ -245,6 +245,16 @@ export type AUTHOR_BY_GITHUB_ID_QUERYResult = {
   email: string | null;
   bio: string | null;
 } | null;
+// Variable: AUTHOR_BY_ID_QUERY
+// Query: *[_type == "author" && _id == $id][0]{        _id,        name,        username,        image,        email,        bio    }
+export type AUTHOR_BY_ID_QUERYResult = {
+  _id: string;
+  name: string | null;
+  username: string | null;
+  image: string | null;
+  email: string | null;
+  bio: string | null;
+} | null;
 
 // Query TypeMap
 import "@sanity/client";
@@ -254,5 +264,6 @@ declare module "@sanity/client" {
     "\n    *[_type == \"startup\" && _id == $id][0]  {\n        _id, \n        title,\n        slug,\n        _createdAt,\n        author -> {\n            _id, \n            name, \n          username,\n            image, \n            bio\n        },\n        views,\n        description,\n        category,\n        image,\n        pitch\n    }    \n": STARTUP_BY_ID_QUERYResult;
     "\n    *[_type == \"startup\" && _id == $id][0]  {\n        _id, \n        views,\n    }    \n": STARTUP_VIEWS_QUERYResult;
     "\n    *[_type == \"author\" && id == $id][0]{\n        _id,\n        name,\n        username,\n        image,\n        email,\n        bio\n    }\n    ": AUTHOR_BY_GITHUB_ID_QUERYResult;
+    "\n    *[_type == \"author\" && _id == $id][0]{\n        _id,\n        name,\n        username,\n        image,\n        email,\n        bio\n    }\n    ": AUTHOR_BY_ID_QUERYResult;
   }
 }
