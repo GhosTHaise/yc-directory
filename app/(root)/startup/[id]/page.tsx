a4fb1500b5dd1a035c7c9d831ed0cb2994f93d23
Fitiavana Sambatra <Ghostrex2@gmail.com>
2024-11-22 18:41:09 +0300

    feat: fetch playlist

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
-import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/query";
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from "@/sanity/lib/query";
 import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
@@ -8,6 +8,7 @@ import makdownit from "markdown-it"
 import { Suspense } from "react";
 import View from "@/components/view";
 import { Skeleton } from "@/components/ui/skeleton";
+import StartupCard, { StartupTypeCard } from "@/components/startupCard";
 
 export const experimental_ppr = true
 
@@ -15,32 +16,34 @@ const md = makdownit();
 
 const StartupPage = async ({ params }: { params: Promise<{ id: string }> }) => {
     const id = (await params).id;
-    const post =  await  client.fetch(STARTUP_BY_ID_QUERY,{id});
+    const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
 
-    if(!post) return notFound();
+    const { select: editorPosts } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: "editor-picks-news" })
+
+    if (!post) return notFound();
 
     const parsedContent = md.render(post?.pitch || '')
 
     return (
         <>
-        <section className="pink_container !min-h-[230px]">
-            <p className="tag">
-                {
-                    formatDate(post._createdAt)
-                }
-            </p>
+            <section className="pink_container !min-h-[230px]">
+                <p className="tag">
+                    {
+                        formatDate(post._createdAt)
+                    }
+                </p>
 
-            <h1 className="heading">
-                {post.title}
-            </h1>
+                <h1 className="heading">
+                    {post.title}
+                </h1>
 
-            <p className="sub-heading !max-w-5xl">
-                {
-                    post.description
-                }
-            </p>
-        </section>
-           <section className="section_container">
+                <p className="sub-heading !max-w-5xl">
+                    {
+                        post.description
+                    }
+                </p>
+            </section>
+            <section className="section_container">
                 <img
                     src={post.image!}
                     alt="thumbnail"
@@ -52,7 +55,7 @@ const StartupPage = async ({ params }: { params: Promise<{ id: string }> }) => {
                             href={`/user/${post.author?._id}`}
                             className="flex gap-2 items-center mb-3"
                         >
-                            <Image 
+                            <Image
                                 src={post.author?.image!}
                                 alt="avatar"
                                 width={64}
@@ -95,14 +98,30 @@ const StartupPage = async ({ params }: { params: Promise<{ id: string }> }) => {
 
                 <hr className="divider" />
 
-                {/*TODO : EDITOR SELECTED STARTUPS */}
+                {
+                    editorPosts?.length > 0 && (
+                        <div className="max-w-4xl mx-auto">
+                            <p className="text-30-semibold">
+                                Editor Picks
+                            </p>
+
+                            <ul className="mt-7 card_grid-sm">
+                                {
+                                    editorPosts.map((post : StartupTypeCard , index : number) => (
+                                        <StartupCard key={index} post={post} />
+                                    ))
+                                }
+                            </ul>
+                        </div>
+                    )
+                }
 
                 <Suspense fallback={
                     <Skeleton className="view_skeleton" />
                 }>
                     <View id={id} />
                 </Suspense>
-           </section>
+            </section>
         </>
     )
 }
